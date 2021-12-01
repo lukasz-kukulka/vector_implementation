7f1b3b837dda5b71323b66f275e4d52515235e3a
lukasz-kukulka <qqla83@gmail.com>
2021-12-01 03:39:33 +0000

    add vector cpp

---
+++ CMakeFiles/Vector-lib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Vector-lib.dir/vector.cpp.o"
+  "CMakeFiles/Vector-lib.dir/vector.cpp.o.d"
+  "libVector-lib.a"
+  "libVector-lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Vector-lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
