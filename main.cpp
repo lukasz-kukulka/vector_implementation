693c639d7fc71b3a72500a0ced3774eef41ea9f2
lukasz-kukulka <qqla83@gmail.com>
2021-12-16 17:28:19 +0000

    alloc test

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 int main() {
     std::vector<int>v;
     std::cout << v.capacity() << " - " << v.size() << '\n';
+    myVector<int>v1(1);
 
     return 0;
 }
--- myVector.hpp
+++ myVector.hpp
@@ -2,6 +2,7 @@
 
 #include <type_traits>
 #include <memory>
+#include <iostream>
 
 template<typename Type, class Allocator = std::allocator<Type>>
 class myVector {
@@ -10,9 +11,17 @@ public:
         
     }
     myVector(size_t size) {
-        alloc_.allocate(size);
+        poiterAlloc_ = alloc_.allocate(size);
+        // std::cout << "poiterAlloc_ = " << poiterAlloc_[0] << "       *poiterAlloc_ = " << *poiterAlloc_ << "        &poiterAlloc_ = " << &poiterAlloc_[0] << '\n';
+        // using traits_t = std::allocator_traits<decltype(alloc_)>;
+        // traits_t::construct(alloc_, poiterAlloc_ + 1, 7);   
+        // std::cout << "poiterAlloc_ = " << poiterAlloc_[1] << "       *poiterAlloc_ = " << *poiterAlloc_ + 1 << "        &poiterAlloc_ = " << &poiterAlloc_[1] << '\n';
+        // traits_t::construct(alloc_, poiterAlloc_ + 2, 22);   
+        // std::cout << "poiterAlloc_ = " << poiterAlloc_[2] << "       *poiterAlloc_ = " << *poiterAlloc_ + 2 << "        &poiterAlloc_ = " << &poiterAlloc_[2] << '\n';
     }
-    ~myVector();
+    ~myVector() {
+        alloc_.deallocate(poiterAlloc_, capacity_);
+    }
 private:
     size_t size_ {};
     size_t capacity_ {};
