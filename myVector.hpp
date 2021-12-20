d20f11142f55a968420d63bbbc00bb2e1ed98a6d
lukasz-kukulka <qqla83@gmail.com>
2021-12-20 15:51:27 +0000

    add capacity

--- myVector.hpp
+++ myVector.hpp
@@ -1,5 +1,13 @@
 #pragma once
 
+        // std::cout << "poiterAlloc_ = " << poiterAlloc_[0] << "       *poiterAlloc_ = " << *poiterAlloc_ << "        &poiterAlloc_ = " << &poiterAlloc_[0] << '\n';
+        // traits_t::construct(alloc_, poiterAlloc_ + 1000, 7);   
+        // std::cout << "poiterAlloc_ = " << poiterAlloc_[1] << "       *poiterAlloc_ = " << *poiterAlloc_ + 1 << "        &poiterAlloc_ = " << &poiterAlloc_[1000] << '\n';
+        // traits_t::construct(alloc_, poiterAlloc_ + 2, 22);   
+        // std::cout << "poiterAlloc_ = " << poiterAlloc_[2] << "       *poiterAlloc_ = " << *poiterAlloc_ + 2 << "        &poiterAlloc_ = " << &poiterAlloc_[2] << '\n';  
+        // poiterAlloc_ = traits_t::allocate(alloc_, size); 
+        // std::cout << "poiterAlloc_ = " << poiterAlloc_[3] << "       *poiterAlloc_ = " << *poiterAlloc_<< "        &poiterAlloc_ = " << &poiterAlloc_[3] << '\n';
+
 #include <type_traits>
 #include <memory>
 #include <iostream>
@@ -11,19 +19,18 @@ public:
         
     }
     myVector(size_t size) {
-        poiterAlloc_ = alloc_.allocate(size);
-        // std::cout << "poiterAlloc_ = " << poiterAlloc_[0] << "       *poiterAlloc_ = " << *poiterAlloc_ << "        &poiterAlloc_ = " << &poiterAlloc_[0] << '\n';
-        // using traits_t = std::allocator_traits<decltype(alloc_)>;
-        // traits_t::construct(alloc_, poiterAlloc_ + 1, 7);   
-        // std::cout << "poiterAlloc_ = " << poiterAlloc_[1] << "       *poiterAlloc_ = " << *poiterAlloc_ + 1 << "        &poiterAlloc_ = " << &poiterAlloc_[1] << '\n';
-        // traits_t::construct(alloc_, poiterAlloc_ + 2, 22);   
-        // std::cout << "poiterAlloc_ = " << poiterAlloc_[2] << "       *poiterAlloc_ = " << *poiterAlloc_ + 2 << "        &poiterAlloc_ = " << &poiterAlloc_[2] << '\n';
+        capacity_ += size;
+        using traits_t = std::allocator_traits<decltype(alloc_)>;
+        poiterAlloc_ = traits_t::allocate(alloc_, size);
     }
     ~myVector() {
         alloc_.deallocate(poiterAlloc_, capacity_);
     }
+
+    size_t capacity() {
+        return capacity_;
+    }
 private:
-    size_t size_ {};
     size_t capacity_ {};
     std::allocator<Type>alloc_;
     Type* poiterAlloc_ { nullptr };
