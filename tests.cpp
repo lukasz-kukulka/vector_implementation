e53112a960f42d9aa83d45f37e791eb47ca210d9
lukasz-kukulka <qqla83@gmail.com>
2022-01-26 17:43:34 +0000

    add vector class string with size [TESTS]

--- tests.cpp
+++ tests.cpp
@@ -23,9 +23,11 @@ struct TestsVectorClass : public ::testing::Test {
     luk::Vector<testClassToVector<int>>intVectorSize { luk::Vector<testClassToVector<int>>(50) };
     luk::Vector<testClassToVector<int>>intVectorSizeValue { luk::Vector<testClassToVector<int>>(99, 111) };
     luk::Vector<testClassToVector<int>>intVectorInitList { luk::Vector<testClassToVector<int>>({222, 223, 224, 225, 226, 227, 228, 229, 230, 231})};
+    luk::Vector<testClassToVector<std::string>>intVectorSize { luk::Vector<testClassToVector<std::string>>(100) };
+
 };
 
-TEST_F(TestsVectorClass, allValueInVectorFromConstructorWithSizeShouldBeZero) {
+TEST_F(TestsVectorClass, allValueInIntVectorFromConstructorWithSizeShouldBeZero) {
     auto expect = 0;
     ASSERT_EQ(intVectorSize[0].getValue(), expect);
     ASSERT_EQ(intVectorSize[10].getValue(), expect);
