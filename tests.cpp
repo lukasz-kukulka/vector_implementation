304b9ee8cd0b32f6c88008e13da4679df1c82b46
lukasz-kukulka <qqla83@gmail.com>
2022-01-29 13:44:44 +0000

    add callIntVectorCapacityShouldBeZero [TESTS]

--- tests.cpp
+++ tests.cpp
@@ -23,6 +23,7 @@ struct TestsVectorClass : public ::testing::Test {
     luk::Vector<testClassToVector<int>>intVectorSize { luk::Vector<testClassToVector<int>>(50) };
     luk::Vector<testClassToVector<int>>intVectorSizeValue { luk::Vector<testClassToVector<int>>(99, 111) };
     luk::Vector<testClassToVector<int>>intVectorInitList { luk::Vector<testClassToVector<int>>({222, 223, 224, 225, 226, 227, 228, 229, 230, 231})};
+    luk::Vector<testClassToVector<int>>stringVectorEmpty;
     luk::Vector<testClassToVector<std::string>>stringVectorSizeValue = luk::Vector<testClassToVector<std::string>>(500, std::string("TEST"));
     luk::Vector<testClassToVector<std::string>>stringVectorInitList { {"TEST001"}, {"TEST002"}, {"TEST003"}, {"TEST004"}, {"TEST005"}, {"TEST006"}, {"TEST007"} };
 };
@@ -41,4 +42,9 @@ TEST_F(TestsVectorClass, allValueInIntVectorFromConstructorWithSizeShouldBe111) 
     ASSERT_EQ(intVectorSize[33].getValue(), expect);
     ASSERT_EQ(intVectorSize[66].getValue(), expect);
     ASSERT_EQ(intVectorSize[98].getValue(), expect);
+}
+
+TEST_F(TestsVectorClass, callIntVectorCapacityShouldBeZero) {
+    auto expect = 0;
+    ASSERT_EQ(intVectorEmpty.capacity(), expect);
 }
